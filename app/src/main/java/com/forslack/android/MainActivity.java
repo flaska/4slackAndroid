363e0d4f3f796c40a5d4b515aadb1a8546baea75
Jakub Flaska <jakub.flaska@nih.gov>
2019-01-18 14:39:46 -0500

    x

---
+++ app/src/main/java/com/forslack/android/MainActivity.java
@@ -0,0 +1,18 @@
+package com.forslack.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.4slack.com/");
+    }
+}
